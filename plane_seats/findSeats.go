fcf1b72baf0ce2f7e7625607ad3fdcb9ebe736a7
rizash <sajmerdenr@gmail.com>
2022-09-08 10:43:03 +0600

    I think it is completed

--- plane_seats/findSeats.go
+++ plane_seats/findSeats.go
@@ -1,35 +1,81 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 func findSeats() {
+outer:
 	for _, group := range groups {
-		var rowSeats [6]bool
 		var row int
-		for row, rowSeats = range planeSeats {
-			var start int
-			var backwards bool
-			switch {
-			case group.side == "left" && group.position == "aisle":
-				start = 2
-				backwards = true
-			case group.side == "left" && group.position == "window":
-				start = 0
-			case group.side == "right" && group.position == "aisle":
-				start = 3
-			case group.side == "right" && group.position == "window":
-				start = 5
-				backwards = true
-			default:
-				log.Fatalln("Erroneous input.")
+		seatsAvailable := true
+
+		start, finish := startDirection(group)
+
+		for row = range planeSeats {
+
+			for i := start; i < finish; i++ {
+				if !planeSeats[row][i] {
+					seatsAvailable = false
+				}
 			}
 
-			if !backwards {
+			if seatsAvailable {
+				fmt.Print("Passengers can take seats:")
+				for i := start; i < finish; i++ {
+					fmt.Printf(" %d%c", row+1, rune('A'+i))
+					planeSeats[row][i] = false
+				}
+				fmt.Println()
+				printPlaneSeats(row, start, finish)
 
-			} else {
-
+				continue outer
 			}
+
+			seatsAvailable = true
 		}
 
+		fmt.Println("Cannot fulfill passengers requirements.")
+	}
+}
+
+func startDirection(group passangerGroup) (start, finish int) {
+	switch {
+	case group.side == "left" && group.position == "aisle":
+		start = 3 - group.size
+		finish = 3
+	case group.side == "left" && group.position == "window":
+		start = 0
+		finish = group.size
+	case group.side == "right" && group.position == "aisle":
+		start = 3
+		finish = 3 + group.size
+	case group.side == "right" && group.position == "window":
+		start = 6 - group.size
+		finish = 6
+	default:
+		log.Fatalln("Erroneous input.")
+	}
+
+	return start, finish
+}
+
+func printPlaneSeats(row, start, finish int) {
+	for i, planeRow := range planeSeats {
+		for j, seat := range planeRow {
+			if i == row && j >= start && j < finish {
+				fmt.Print("X")
+			} else if seat {
+				fmt.Print(".")
+			} else {
+				fmt.Print("#")
+			}
+
+			if j == 2 {
+				fmt.Print("_")
+			}
+		}
+		fmt.Println()
 	}
 }
